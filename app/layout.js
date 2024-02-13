b4a9640bb6d4c10cf6226c73e962e4d16fac55db
Tesimune <tesimune@gmail.com>
2024-02-13 08:33:34 +0100

    Added favicon

--- app/layout.js
+++ app/layout.js
@@ -14,6 +14,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="icon"
+          href="https://www.hostshifts.com/logo.png"
+          sizes="any"
+        />
+      </head>
       <body className={inter.className}>
         <ThemeProvider>{children}</ThemeProvider>
       </body>
